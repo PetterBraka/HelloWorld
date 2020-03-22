d2b3982bab535d91796a21ce67e0300c5b68ceea
PetterBraka <petter.braka@gmail.com>
2020-03-22 01:42:06 +0100

    I made a small game called cardWar. The point of the game is to get the highest score. I added a deal button that will give you two cards, then give you your score and then give the AI a score. I'm going to add a message to tell you who won the game.
    This is just a simple game to make me understand Swift and the flow of programming with swift.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var aiScore: UILabel!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var leftCard: UIImageView!
+    @IBOutlet weak var rightCard: UIImageView!
+    @IBAction func deal(_ sender: Any) {
+        let cards: [UIImage] = [
+        UIImage(named: "card2.png")!,
+        UIImage(named: "card3.png")!,
+        UIImage(named: "card4.png")!,
+        UIImage(named: "card5.png")!,
+        UIImage(named: "card6.png")!,
+        UIImage(named: "card7.png")!,
+        UIImage(named: "card8.png")!,
+        UIImage(named: "card9.png")!,
+        UIImage(named: "card10.png")!,
+        UIImage(named: "card11.png")!,
+        UIImage(named: "card12.png")!,
+        UIImage(named: "card13.png")!,
+        UIImage(named: "card14.png")!]
+        let leftRandom = Int.random(in: 0..<13)
+        var rightRandom = Int.random(in: 0..<13)
+        while rightRandom == leftRandom {
+            rightRandom = Int.random(in: 0..<13)
+        }
+        leftCard.image = cards[leftRandom]
+        rightCard.image = cards[rightRandom]
+        //sice the cards has a the minimum value of 2 I add 4. I need to add 4 because i im using two cards and then since the value of the random vals will be 2 less than the card its showing i need to add 4
+        let player = leftRandom + 4 +  rightRandom
+        playerScore.text =
+            String.init(player)
+        let ai = Int.random(in: 5..<28)
+        aiScore.text = String.init(ai)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
